cd67093c064689e97335ca13c7d454e0f57cdb1a
Eli <elijw22@gmail.com>
2022-10-01 12:33:42 -0400

    Tasks!

--- Assets/Scripts/Tasks/Task.cs
+++ Assets/Scripts/Tasks/Task.cs
@@ -16,74 +16,156 @@ public class Task : MonoBehaviour
     private bool taskUp = false;
     private int keyIndex = 0;
 
+    private PromptSpot actualSpot;
+    private GameObject promptPrefab;
+    private GameObject prompt;
+
+    public Color successColor = Color.green;
+    public Color failColor = Color.red;
+
     
     private float interval() 
     {
         return Random.Range(minInterval, maxInterval);
     }
 
-    private void resetTask()
+    private void ResetTask()
     {
         timeTillStart = interval();
         taskUp = false;
+        Destroy(prompt);
     }
 
-    private void failTask()
+    private void FailTask()
     {
         Debug.Log("Task Failed");
-        resetTask();
+        taskUp = false;
+        StartCoroutine(FailTaskAnim());
         //Failure Stuff
     }
+    private IEnumerator FailTaskAnim()
+    {
+        for (int i = 0; i < keys.Length; i++)
+        {
+            SpriteRenderer keySR = prompt.transform.Find((i+1).ToString()).gameObject.GetComponent<SpriteRenderer>();
+            keySR.color = failColor;
+        }
+        yield return new WaitForSeconds(0.6f);
+        EndTask();
+    }
 
-    private void startTask() 
+    private void SucceedTask()
+    {
+        Debug.Log("Task Succeeded");
+        taskUp = false;
+        StartCoroutine(SucceedTaskAnim());
+    }
+    private IEnumerator SucceedTaskAnim()
+    {
+        for (int i = 0; i < keys.Length; i++)
+        {
+            SpriteRenderer keySR = prompt.transform.Find((i+1).ToString()).gameObject.GetComponent<SpriteRenderer>();
+            keySR.color = successColor;
+        }
+        yield return new WaitForSeconds(0.6f);
+        EndTask();
+    }
+
+    private void StartTask() 
     {
         Debug.Log("Task Started");
         //Display Task
+        
+        PromptSpot[] spots = transform.Find("TrainPlane").gameObject.GetComponentsInChildren<PromptSpot>();
+        actualSpot = null;
+        foreach (PromptSpot spot in spots)
+        {
+            if (!spot.taken)
+            {
+                actualSpot = spot;
+                spot.taken = true;
+                break;
+            }
+        }
+        if (actualSpot is null)
+        {
+            //Couldn't find a spot logic goes here
+            Debug.Log("Can't find a spot");
+            return;
+        }
+
+        prompt = Instantiate(promptPrefab, actualSpot.transform);
+        for (int i = 0; i < keys.Length; i++)
+        {
+            //Assigne the correct sprites to the prefab
+            string path = "Sprites/Keyboard_White_"+keys[i].ToString();
+            Sprite sprite = Resources.Load<Sprite>(path);
+            Transform t = prompt.transform.Find((i+1).ToString());
+            t.gameObject.GetComponent<SpriteRenderer>().sprite = sprite;
+
+        }
+
         timeTillStart = interval();
         timeTillFail = failTime;
         taskUp = true;
         keyIndex = 0;
     }
 
-    private void endTask()
+    private void EndTask()
     {
         Debug.Log("Task Completed!");
-        resetTask();
+        taskUp = false;
+        actualSpot.taken = false;
+        actualSpot = null;
+        ResetTask();
     }
 
-    private void keySuccess()
+    private void KeySuccess()
     {
         Debug.Log("Correct Key");
         //Do positive feedback here
+        prompt.transform.Find((keyIndex+1).ToString()).gameObject.GetComponent<SpriteRenderer>().color = successColor;
         keyIndex++;
-        if (keyIndex >= keys.Length) endTask();
+        if (keyIndex >= keys.Length) SucceedTask();
     }
 
-    private void keyFail()
+    private void KeyFail()
     {
         Debug.Log("Incorrect Key");
         //Do negative feedback here
+        StartCoroutine(KeyFailAnim(keyIndex));
+
+    }
+    private IEnumerator KeyFailAnim(int i)
+    {
+        SpriteRenderer keySR = prompt.transform.Find((i+1).ToString()).gameObject.GetComponent<SpriteRenderer>();
+        keySR.color = failColor;
+        yield return new WaitForSeconds(0.3f);
+        keySR.color = Color.white;
     }
 
     void Start()
     {
-        resetTask();
+        ResetTask();
         timeTillFail = failTime;
+
+        if (keys.Length > 5) Debug.LogError("Too many keys!");
+        promptPrefab = Resources.Load<GameObject>("Prefabs/TaskPrompts/"+keys.Length.ToString()+"Key"); //Find correct size prefab
     }
 
     // Update is called once per frame
     void Update()
     {
         timeTillStart -= Time.deltaTime;
-        if (timeTillStart <= 0) startTask();
+        if (timeTillStart <= 0) StartTask();
 
         if (taskUp)
         {
             timeTillFail -= Time.deltaTime;
-            if (timeTillFail <= 0) failTask();
+            if (timeTillFail <= 0) FailTask();
 
-            if (Input.GetKeyDown(keys[keyIndex])) keySuccess();
-            else if (Input.anyKeyDown) keyFail();
+            if (Input.GetKeyDown(keys[keyIndex])) KeySuccess();
+            else if (Input.anyKeyDown) KeyFail();
         }     
     }
 }
